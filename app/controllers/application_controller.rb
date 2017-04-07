2f3f60733cc9b93e3f136ea18e5a3cb169241d54
sherawat42 <manishsherawat1@gmail.com>
2017-04-08 05:05:58 +0530

    temp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render html: "Hello, world!! Manish is here.
-  	 ki haal chaal tumhaare??"
+  	render :hello
   end
 end
