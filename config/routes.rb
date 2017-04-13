5556ee056b4a0381cbf9d7991acd88d8f488afb9
sherawat42 <manishsherawat1@gmail.com>
2017-04-14 01:13:36 +0530

    done_some_work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # get 'users/new'
 
   get 'sign_up', to: 'users#new'
@@ -10,4 +12,9 @@ Rails.application.routes.draw do
   resources :microposts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
 end
